74aa7207fced0c3db5734d5e2e9a2c306a30a3f4
kamlendu <kamlendup@gmail.com>
2025-09-11 10:16:34 +0530

    java

---
+++ TestEJBApp/src/main/java/ejb/LocalBean.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB40/StatelessEjbClass.java to edit this template
+ */
+package ejb;
+
+import jakarta.ejb.Stateless;
+
+/**
+ *
+ * @author kamlendu
+ */
+@Stateless(mappedName = "ejb/lb")
+public class LocalBean implements LocalBeanLocal {
+
+    @Override
+    public String sayLocalHello() {
+        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+   
+    
+    return "Hello EJB from a Local Bean";
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+
+
+}
